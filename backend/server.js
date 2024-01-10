edc12a689456611fe5e1e0efe9b0154660495885
ankyson23 <aniket.sonawane1734@gmail.com>
2024-01-11 01:05:31 +0530

    everything normal

---
+++ backend/server.js
@@ -0,0 +1,100 @@
+require("dotenv").config();
+
+
+
+const bodyParser = require("body-parser"); // Middleware
+const express = require("express")
+const serverless = require("serverless-http")
+const socketIO = require("socket.io");
+const http = require("http");
+const path = require("path");
+const mognoose = require("mongoose");
+const { default: mongoose } = require("mongoose");
+const User = require("./modals/userSchema");
+const port = process.env.PORT || 3000;
+const router = express.Router();
+
+let app = express();
+app.use(express.static(path.join(__dirname, "/frontend")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/",router)
+let server = http.createServer(app);
+let io = socketIO(server);
+
+let allUsers = {};
+
+// make connection with user from server side
+io.on("connection", (socket) => {
+  const userid = socket.id;
+  let username = "";
+
+  // Registering new User
+  socket.on("register", (newusername) => {
+    username = newusername;
+    allUsers[username] = userid;
+
+    console.log(`[+] ${username} / ${userid}  Connected`);
+
+    //Tell other users new user joined
+    socket.broadcast.emit(
+      "updateUser",
+      Object.keys(allUsers),
+      `${username} Connected to Server !`
+    );
+
+    socket.emit("updateUser", Object.keys(allUsers), "Welcome to IntraChat !");
+  });
+
+  // listen for message from user
+  socket.on("fromUser", (newMessage) => {
+    const recipentName = newMessage.to;
+    const recipentId = allUsers[recipentName];
+
+    io.to(recipentId).emit("fromServer", newMessage);
+  });
+
+  // when server disconnects from user
+  socket.on("disconnect", () => {
+    console.log("disconnected from " + username);
+    delete allUsers[username];
+
+    // Tell other users  user disconnected
+    socket.broadcast.emit(
+      "updateUser",
+      Object.keys(allUsers),
+      `${username} disconnected !`
+    );
+  });
+});
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/login.html"));
+});
+
+app.use("/.netlify/functions/", router);
+
+module.exports= serverless(app);
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("[+] Connected to Database.");
+
+    server.listen(port, () => {
+      console.log(`Server started on http://localhost:${port}/\n`);
+    });
+  })
+  .catch((err) => {
+    console.log("[-] Error : " + err);
+  });
+
+// app.post("/", async (req, res) => {
+//   const userdata = await User.find({});
+//   console.log(userdata);
+//   const { username, password } = req.body;
+//   res.redirect(`/user/${username}/password/${password}`);
+// });
+
+// app.get("/user/:username", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/client.html"));
+// });
