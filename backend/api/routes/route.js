94521f2b4716d73fd6bddaa18a6784590c5784c8
Aniket Sonawane <aniket.sonawane1734@gmail.com>
2024-07-26 11:00:00 +0530

    aws

--- backend/api/routes/route.js
+++ backend/api/routes/route.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const { loginPage,loginHandler ,chatPage, registerHandler , searchUser}= require("./routeController")
 
-// router.get("/", loginPage);
+router.get("/", loginPage);
 
-// router.get("/chat/",(chatPage))
+router.get("/chat/",(chatPage))
 
 router.post("/api/login",loginHandler)
 router.post("/api/register",registerHandler)
--- backend/api/routes/routeController.js
+++ backend/api/routes/routeController.js
@@ -3,7 +3,6 @@ const User = require("../modals/userSchema");
 const bcrypt = require("bcrypt");
 
 function loginPage(req, res) {
-  console.log("hello nigger")
   res.sendFile(path.join(__dirname, "../public/login.html"));
 }
 
