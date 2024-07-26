de3b4ad4a8639dc7c71dba4931b29576320907fd
Aniket Sonawane <aniket.sonawane1734@gmail.com>
2024-07-27 00:34:58 +0530

    simpliefied

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,36 +1,21 @@
 require("dotenv").config();
-const socketIO = require("socket.io");
-
-const cors = require("cors");
 const express = require("express");
-const serverless = require("serverless-http");
-const http = require("http");
-const router = require("../routes/route.js");
-const socketHandle = require("./sockets/socketHandler.js");
-const startServer = require("./serverStart.js");
+const cors = require("cors");
 
 const app = express();
-const server = http.createServer(app);
-// const io = socketIO(server, {
-//   cors: {
-//     origin: "https://intra-chat.vercel.app",
-//     methods: ["GET", "POST"],
-//   },
-// });
 
-// app.use(
-//     cors({
-//     origin: "https://intra-chat.vercel.app",
-// })
-// );
+app.use(cors({
+  origin: "https://intra-chat.vercel.app",
+}));
+
 app.use(express.json());
-app.use("/api", router);
 
 app.get("/", (req, res) => {
-    console.log("Hello1");
-    res.send("<h1>Nigga why</h1>").status(200);
+  console.log("Hello");
+  res.send("<h1>Hello World</h1>");
 });
-// socketHandle(io);
 
-module.exports = serverless(app);
-// startServer(server);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
