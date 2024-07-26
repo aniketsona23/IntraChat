84f129591adc27e8e153b2daf1ff82661c2b6212
Aniket Sonawane <aniket.sonawane1734@gmail.com>
2024-07-26 22:52:53 +0530

    changed path

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -11,11 +11,11 @@ router.get("/",(req,res)=>{
   res.send("<h1>HEllo</h1>")
 })
 
-router.get("/api",(req,res)=>{
+router.get("/",(req,res)=>{
   res.json({msg:"hellooo"})
 })
-router.post("/api/login", loginHandler);
-router.post("/api/register", registerHandler);
+router.post("/login", loginHandler);
+router.post("/register", registerHandler);
 router.get("/searchuser/:searchname", searchUser);
 
 module.exports = router;
